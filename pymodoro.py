7d0cc62324b708cdcf2e2ac3615ed4a802d4753d
John Tyree <johntyree@gmail.com>
2011-09-28 16:10:05 +0200

    Assume utf-8 instead of latin-1 encoding.

--- pymodoro.py
+++ pymodoro.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 # authors: Dat Chu <dattanchu@gmail.com>
 #          Dominik Mayer <dominik.mayer@gmail.com>
 # Prerequisite
