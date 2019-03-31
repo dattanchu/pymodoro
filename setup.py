1e6aefb698e55bb26d0106b412635cd8be88028f
Denis Kasak <dkasak@termina.org.uk>
2019-03-31 18:16:53 +0200

    Bump version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pymodoro',
-    version='0.3',
+    version='0.4',
     packages=['pymodoro'],
     package_data={'pymodoro': ['data/*']},
     entry_points={
